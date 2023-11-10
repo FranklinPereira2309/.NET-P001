8bc72ec583b1c1b512e390949bd71870fb6e5caa
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-11-10 20:17:42 -0300

    feat:conclusão da atividade2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+        Console.WriteLine("Valores mínimos e máximos dos tipos de dados numéricos inteiros em C#:");
+
+        Console.WriteLine($"sbyte: {sbyte.MinValue} a {sbyte.MaxValue}");
+        Console.WriteLine($"byte: {byte.MinValue} a {byte.MaxValue}");
+        Console.WriteLine($"short: {short.MinValue} a {short.MaxValue}");
+        Console.WriteLine($"ushort: {ushort.MinValue} a {ushort.MaxValue}");
+        Console.WriteLine($"int: {int.MinValue} a {int.MaxValue}");
+        Console.WriteLine($"uint: {uint.MinValue} a {uint.MaxValue}");
+        Console.WriteLine($"long: {long.MinValue} a {long.MaxValue}");
+        Console.WriteLine($"ulong: {ulong.MinValue} a {ulong.MaxValue}");
+
