0e2ee660a265c57d6ac538246af5f23178143029
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-11-10 22:14:37 -0300

    feat:conclusão das atividades

--- program.cs
+++ program.cs
@@ -1,6 +1,10 @@
 ﻿
-        //parte 2
-        /*Console.WriteLine("Valores mínimos e máximos dos tipos de dados numéricos inteiros em C#:");
+        Console.WriteLine();
+        Console.WriteLine("Parte 2:");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Valores mínimos e máximos dos tipos de dados numéricos inteiros em C#:");
 
         Console.WriteLine($"sbyte: {sbyte.MinValue} a {sbyte.MaxValue}");
         Console.WriteLine($"byte: {byte.MinValue} a {byte.MaxValue}");
@@ -9,61 +13,82 @@
         Console.WriteLine($"int: {int.MinValue} a {int.MaxValue}");
         Console.WriteLine($"uint: {uint.MinValue} a {uint.MaxValue}");
         Console.WriteLine($"long: {long.MinValue} a {long.MaxValue}");
-        Console.WriteLine($"ulong: {ulong.MinValue} a {ulong.MaxValue}");*/
+        Console.WriteLine($"ulong: {ulong.MinValue} a {ulong.MaxValue}");
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine();
 
-        //parte 3
-        /*double numeroDouble = 10.9; 
-        int numeroCasting = (int)numeroDouble; // convertido sem a parte fracionaria
-        int numeroCastingArred = (int)Math.Round(numeroDouble); // A solução seria o aredondar 
+        Console.WriteLine("Parte 3:");
+        
+        Console.WriteLine();
+
+        double numeroDouble = 10.9;
+        int numeroCasting = (int)numeroDouble; // Convertido sem a parte fracionária
+        int numeroCastingArred = (int)Math.Round(numeroDouble); // Solução com arredondamento
 
         Console.WriteLine($"Número double: {numeroDouble}");
-        Console.WriteLine($"Número double convertido com a perda da parte fracionária: {numeroCasting}");
-        Console.WriteLine($"Número inteiro convertido com arredondamento: {numeroCastingArred}");*/
- 
-        //parte 4
+        Console.WriteLine($"Número double convertido sem a parte fracionária: {numeroCasting}");
+        Console.WriteLine($"Número inteiro convertido com arredondamento: {numeroCastingArred}");
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine();
 
-        /*int x = 10;
+        Console.WriteLine("Parte 4:");
+        
+        Console.WriteLine();
+
+        int x = 10;
         int y = 2;
         int resultado;
 
         Console.WriteLine($"Soma: {resultado = x + y}");
         Console.WriteLine($"Subtração: {resultado = x - y}");
         Console.WriteLine($"Divisão: {resultado = x / y}");
-        Console.WriteLine($"Multiplicação: {resultado = x * y}");*/
+        Console.WriteLine($"Multiplicação: {resultado = x * y}");
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine();
 
-        //parte 5
+        Console.WriteLine("Parte 5:");
+        
+        Console.WriteLine();
 
-        /*int a = 5;
+        int a = 5;
         int b = 8;
 
         if (a > b)
         {
-                Console.WriteLine("a EH maior que b");
+            Console.WriteLine("a é maior que b");
         }
         else if (a < b)
         {
-                Console.WriteLine("a EH menor que b");
+            Console.WriteLine("a é menor que b");
         }
         else if (a == b)
         {
-                Console.WriteLine("a EH igual b");
-        }*/
+            Console.WriteLine("a é igual a b");
+        }
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine();
 
-        //parte 6
+        Console.WriteLine("Parte 6:");
+        
+        Console.WriteLine();
 
-        /*string str1 = "Hello";
+        string str1 = "Hello";
         string str2 = "World";
 
-        if(str1.Equals(str2)) 
+        if (str1.Equals(str2))
         {
-                Console.WriteLine("As strings SÃO iguais");
+            Console.WriteLine("As strings SÃO iguais");
         }
-        else 
+        else
         {
-                Console.WriteLine("As strings NÃO são iguais");
-        }*/
+            Console.WriteLine("As strings NÃO são iguais");
+        }
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine();
 
-        //parte 7
+        Console.WriteLine("Parte 7:");
+        
+        Console.WriteLine();
 
         bool condicao1 = true;
         bool condicao2 = false;
@@ -74,5 +99,23 @@
         }
         else
         {
-            Console.WriteLine("Uma das condições não é verdadeira.");
+            Console.WriteLine("Pelo menos uma das condições não é verdadeira.");
         }
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Parte 8:");
+        
+        Console.WriteLine();
+
+        int num1 = 7, num2 = 3, num3 = 10;
+
+        if (num1 > num2 && num3 == num1 + num2)
+        {
+            Console.WriteLine("Sim - As condições satisfazem o problema!");
+        }
+        else
+        {
+            Console.WriteLine("Não - As condições NÃO satisfazem o problema!");
+        }
+
