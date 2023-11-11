5a1bcae23506da54da579842565e525da80190c6
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-11-10 21:03:20 -0300

    feat:conclusão atividade3 e atividade4

--- atividade2.cs
+++ atividade2.cs
@@ -1,6 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-        Console.WriteLine("Valores mínimos e máximos dos tipos de dados numéricos inteiros em C#:");
+﻿
+        //parte 2
+        /*Console.WriteLine("Valores mínimos e máximos dos tipos de dados numéricos inteiros em C#:");
 
         Console.WriteLine($"sbyte: {sbyte.MinValue} a {sbyte.MaxValue}");
         Console.WriteLine($"byte: {byte.MinValue} a {byte.MaxValue}");
@@ -9,5 +9,24 @@
         Console.WriteLine($"int: {int.MinValue} a {int.MaxValue}");
         Console.WriteLine($"uint: {uint.MinValue} a {uint.MaxValue}");
         Console.WriteLine($"long: {long.MinValue} a {long.MaxValue}");
-        Console.WriteLine($"ulong: {ulong.MinValue} a {ulong.MaxValue}");
+        Console.WriteLine($"ulong: {ulong.MinValue} a {ulong.MaxValue}");*/
 
+        //parte 3
+        /*double numeroDouble = 10.9; 
+        int numeroCasting = (int)numeroDouble; // convertido sem a parte fracionaria
+        int numeroCastingArred = (int)Math.Round(numeroDouble); // A solução seria o aredondar 
+
+        Console.WriteLine($"Número double: {numeroDouble}");
+        Console.WriteLine($"Número double convertido com a perda da parte fracionária: {numeroCasting}");
+        Console.WriteLine($"Número inteiro convertido com arredondamento: {numeroCastingArred}");*/
+ 
+        //parte 4
+
+        int x = 10;
+        int y = 2;
+        int resultado;
+
+        Console.WriteLine($"Soma: {resultado = x + y}");
+        Console.WriteLine($"Subtração: {resultado = x - y}");
+        Console.WriteLine($"Divisão: {resultado = x / y}");
+        Console.WriteLine($"Multiplicação: {resultado = x * y}");
